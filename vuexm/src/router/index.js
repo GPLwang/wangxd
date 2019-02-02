deea57b62605bd0593517f6aefde5e6f53134c9c
GPLwang <wangxudongge@qq.com>
2019-02-02 10:05:01 +0800

    1
    1

--- vuexm/src/router/index.js
+++ vuexm/src/router/index.js
@@ -1,11 +1,19 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+<<<<<<< HEAD
 import HelloWorld from '@/components/HelloWorld'
 
+=======
+import Login from '@/components/login/login.vue'
+import Home from '@/components/page/home.vue'
+
+import { Message } from 'element-ui'
+>>>>>>> parent of ab38048... home
 Vue.use(Router)
 
 export default new Router({
   routes: [
+<<<<<<< HEAD
     {
       path: '/',
       name: 'HelloWorld',
@@ -13,3 +21,25 @@ export default new Router({
     }
   ]
 })
+=======
+    { path: '/', redirect: '/home' },
+    { name: 'login', path: '/login', component: Login },
+    { name:'home',path:'/home',component:Home }
+  ]
+})
+router.beforeEach((to, from, next) => {
+  if (to.path != '/login') {
+    var token = window.localStorage.getItem('token')
+    if (!token) {
+      router.push('/login')
+      Message.error('请先进行登录在操作')
+    }else{
+      next()
+    }
+  }else{
+    next()
+  }
+})
+
+export default router
+>>>>>>> parent of ab38048... home
